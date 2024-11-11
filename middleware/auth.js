3d3e68c772ef4c2b87b4dfc5ea56de57e8bf3f95
manucharyansos <sosmanucharyan89@gmail.com>
2024-11-11 15:48:30 +0400

    middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,12 @@
 export default function ({ app, redirect, route }) {
   const accessiblePages = ['/', '/contact', '/services'];
 
-  if (!app.$auth.loggedIn && accessiblePages.includes(route.path)) {
-    return;
-  }
-
-  if (!app.$auth.loggedIn && route.path !== '/login') {
-    return redirect('/login');
-  }
-
   if (app.$auth.loggedIn && route.path === '/login') {
     return redirect('/');
+  }
+
+  if (!app.$auth.loggedIn && route.path !== '/login' && !accessiblePages.includes(route.path)) {
+    return redirect('/login');
   }
 
   const userRole = app.$auth.user?.role?.name;
@@ -28,7 +24,7 @@ export default function ({ app, redirect, route }) {
     targetRoute = '/factory/bend';
   }
 
-  if (route.path !== targetRoute) {
+  if (app.$auth.loggedIn && route.path !== targetRoute) {
     return redirect(targetRoute);
   }
 }
