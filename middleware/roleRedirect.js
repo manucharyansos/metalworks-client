bd680f6500576af010c5fcc680f6c935311c6d2b
manucharyansos <sosmanucharyan89@gmail.com>
2024-11-11 04:44:29 +0400

    medsds

--- middleware/roleRedirect.js
+++ middleware/roleRedirect.js
@@ -1,27 +1,42 @@
-export default function ({ app, redirect, route }) {
-  const user = app.$auth.user
-  if (user) {
-    const creatorRole = app.$config.creatorRole
-    const adminRole = app.$config.adminRole
-    const laserRole = app.$config.laserRole
-    const bendRole = app.$config.bendRole
-    const userRole = user.role.name
+  export default function ({ app, redirect, route }) {
+    // Skip the redirection for 'contact' and 'services' routes
+    if (['contact', 'services'].includes(route.name)) {
+      return;
+    }
 
-    let targetRoute = '/'
+    const user = app.$auth.user;
 
-    if (userRole) {
-      if (userRole === creatorRole) {
-        targetRoute = '/creator'
-      } else if (userRole === adminRole) {
-        targetRoute = '/admin'
-      } else if (userRole === laserRole) {
-        targetRoute = '/factory/laser'
-      } else if (userRole === bendRole) {
-        targetRoute = '/factory/bend'
-      }
-      if (route.path !== targetRoute) {
-        return redirect(targetRoute)
-      }
+    // If user is not authenticated, redirect to login page
+    if (!user) {
+      return redirect('/login');
+    }
+
+    // Retrieve roles from app configuration
+    const creatorRole = app.$config.creatorRole;
+    const adminRole = app.$config.adminRole;
+    const laserRole = app.$config.laserRole;
+    const bendRole = app.$config.bendRole;
+
+    // Get the user's role
+    const userRole = user.role.name;
+
+    // Default target route
+    let targetRoute = '/';
+
+    // Determine target route based on user role
+    if (userRole === creatorRole) {
+      targetRoute = '/creator';
+    } else if (userRole === adminRole) {
+      targetRoute = '/admin';
+    } else if (userRole === laserRole) {
+      targetRoute = '/factory/laser';
+    } else if (userRole === bendRole) {
+      targetRoute = '/factory/bend';
+    }
+
+    // Redirect only if the current route is not the target route
+    if (route.path !== targetRoute) {
+      return redirect(targetRoute);
     }
   }
-}
+
