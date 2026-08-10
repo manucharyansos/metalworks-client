9e01804d71c4203c5911e52170228010fd43a1c0
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-10 19:10:26 +0400

    route shared file URL helper through authenticated access

--- plugins/url-helper.js
+++ plugins/url-helper.js
@@ -1,5 +1,18 @@
 export default ({ $axios }, inject) => {
-  const getFileUrl = (filePath) =>
-    `${$axios.defaults.baseURL}/api/storage/${filePath}`
+  const getFileUrl = (filePath) => {
+    if (!filePath) return null
+
+    const baseURL = String($axios.defaults.baseURL || '').replace(/\/+$/, '')
+    const normalizedPath = String(filePath)
+      .replace(/\\/g, '/')
+      .replace(/^\/+/, '')
+    const encodedPath = normalizedPath
+      .split('/')
+      .map((segment) => encodeURIComponent(segment))
+      .join('/')
+
+    return `${baseURL}/api/secure-files/path/${encodedPath}`
+  }
+
   inject('getFileUrl', getFileUrl)
 }
