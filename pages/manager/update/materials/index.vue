e9ba10c717cea45d623d0eea4ca4f4c9793240b4
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-11 23:02:27 +0400

    retire legacy material update page

--- pages/manager/update/materials/index.vue
+++ pages/manager/update/materials/index.vue
@@ -1,10 +1,11 @@
-<template>
-  <div>Materials</div>
-</template>
+<template><div class="min-h-[40vh]"></div></template>
 <script>
 export default {
   layout: 'manager',
   middleware: ['role-guard'],
   meta: { role: 'manager' },
+  mounted() {
+    this.$router.replace(this.localePath('/manager/materials'))
+  },
 }
 </script>
