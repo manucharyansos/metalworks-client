2bf991c02f05849622e4a996dc87ab1844ce8260
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-09-12 19:24:06 +0400

    Expose language switcher on public pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,12 @@
 <template>
-  <main class="flex flex-col min-h-screen items-center justify-center">
+  <main class="relative flex min-h-screen flex-col items-center justify-center">
+    <div class="fixed right-4 top-4 z-[1200] rounded-2xl border border-slate-200 bg-white/90 p-1.5 shadow-lg backdrop-blur dark:border-slate-700 dark:bg-slate-900/90 sm:right-6 sm:top-6">
+      <LanguageDropdown />
+    </div>
     <Nuxt />
   </main>
 </template>
 
 <script>
-export default {}
+export default { name: 'DefaultLayout' }
 </script>
-
-<style scoped></style>
