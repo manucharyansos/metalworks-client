d075c7f42a5054c72c6a861a98c5876f505bc557
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-09-12 15:11:47 +0400

    Expose user management in manager workspace

---
+++ pages/manager/users/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <AdminUsersPage />
+</template>
+
+<script>
+import AdminUsersPage from '@/pages/admin/users/index.vue'
+
+export default {
+  name: 'ManagerUsersPage',
+  components: { AdminUsersPage },
+  layout: 'manager',
+  middleware: ['role-guard'],
+  meta: { role: 'manager' },
+  async asyncData({ store }) {
+    await Promise.all([
+      store.dispatch('users/fetchUsers'),
+      store.dispatch('roles/fetchRoles'),
+    ])
+  },
+}
+</script>
